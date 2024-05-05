6dae188dfb13f3ed2e934f7b8e48712f658f2eba
Maizer <vasilbazhiv12@gmail.com>
2024-05-05 15:52:47 +0300

    new hovers effect

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -2,10 +2,12 @@ import styles from './footer.module.scss';
 import Image from 'next/image';
 
 export const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="bg-dark-700 py-1">
       <div className={styles.footer__inner + ' container'}>
-        <p className="text-md text-light-900">©2023 MyApp. All rights reserved</p>
+        <p className="text-md text-light-900">Copyright ©{year} MyApp. All rights reserved</p>
       </div>
     </footer>
   );
