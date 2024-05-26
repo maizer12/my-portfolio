511b5f124388259516eab839113bf82437f193ca
Maizer <vasilbazhiv12@gmail.com>
2024-05-26 22:57:43 +0300

    fix page

--- src/app/[locale]/project/[id]/page.tsx
+++ src/app/[locale]/project/[id]/page.tsx
@@ -1,18 +1,11 @@
 import { HTag } from '@/common';
 
-type Props = {
-  id: string;
-};
-
-const ProjectPage = ({ id }: Props) => {
+export default function Project() {
   return (
     <section className="py-24 bg-main justify-center items-center h-screen">
       <div className="max-w-[1200px] w-full mx-auto banner">
-        <h1>Project ID: {id}</h1>
         <HTag tag="h1">DevFlow:</HTag>
       </div>
     </section>
   );
-};
-
-export default ProjectPage;
+}
